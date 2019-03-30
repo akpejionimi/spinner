335cb4b4212756a8df2aa7fb5cc2b3c888734119
akpejionimi <akpejionimi@yahoo.com>
2019-03-30 13:04:38 +0100

    media queries added and some modification to html and css

---
+++ main.js
@@ -0,0 +1,11 @@
+const menuBar = document.querySelector('#hamb-menu');
+const details =  document.querySelector('#menu');
+
+menuBar.addEventListener('click', ()=> {
+   menuBar.classList.toggle("change");
+   if(details.style.display === 'block'){
+       details.style.display = 'none';
+   }else{
+       details.style.display = 'block';
+   };
+})
